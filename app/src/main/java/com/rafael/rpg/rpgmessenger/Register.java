81d976248f7d36e8d5040bce1d7ea89f998da2ce
RafaelKuebler <rafael_kuebler@yahoo.es>
2017-03-22 15:44:48 +0100

    Created all activity layouts and the corresponding classes.

---
+++ app/src/main/java/com/rafael/rpg/rpgmessenger/Register.java
@@ -0,0 +1,10 @@
+package com.rafael.rpg.rpgmessenger;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rafael on 22/03/2017.
+ */
+
+public class Register extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/rafael/rpg/rpgmessenger/UserDetails.java
@@ -0,0 +1,8 @@
+package com.rafael.rpg.rpgmessenger;
+
+/**
+ * Created by Rafael on 22/03/2017.
+ */
+
+public class UserDetails {
+}
