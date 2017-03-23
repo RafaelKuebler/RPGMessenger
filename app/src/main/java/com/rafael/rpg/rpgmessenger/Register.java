48523b219483280fa062b3951260c0141079a068
rafaelk <rafael_kuebler@yahoo.es>
2017-03-23 17:00:52 +0100

    Deleted old main activity. Updated firebase version to use firebase-auth. Re-implemented the Register activity to register a new user.

--- app/src/main/java/com/rafael/rpg/rpgmessenger/Register.java
+++ app/src/main/java/com/rafael/rpg/rpgmessenger/Register.java
@@ -1,119 +1,126 @@
 package com.rafael.rpg.rpgmessenger;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.firebase.client.Firebase;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
-    EditText username, password;
-    Button registerButton;
-    String user, pass;
-    TextView login;
+    private static final String TAG = "EmailPassword";
+
+    private EditText usernameField, passwordField;
+    private Button registerButton;
+    private TextView loginView;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
+        // get references to fields and views
+        usernameField = (EditText)findViewById(R.id.username);
+        passwordField = (EditText)findViewById(R.id.password);
         registerButton = (Button)findViewById(R.id.registerButton);
-        login = (TextView)findViewById(R.id.login);
+        loginView = (TextView)findViewById(R.id.login);
 
-        Firebase.setAndroidContext(this);
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        login.setOnClickListener(new View.OnClickListener() {
+        // if the user wants to login change back to login activity
+        loginView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Register.this, Login.class));
             }
         });
 
+        // if the user wants to register check input and register him
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                user = username.getText().toString();
-                pass = password.getText().toString();
+                String user = usernameField.getText().toString();
+                String pass = passwordField.getText().toString();
 
                 if(user.equals("")){
-                    username.setError("can't be blank");
+                    usernameField.setError("can't be blank");
                 }
                 else if(pass.equals("")){
-                    password.setError("can't be blank");
+                    passwordField.setError("can't be blank");
                 }
                 else if(!user.matches("[A-Za-z0-9]+")){
-                    username.setError("only alphabet or number allowed");
+                    usernameField.setError("only alphabet or number allowed");
                 }
                 else if(user.length()<5){
-                    username.setError("at least 5 characters long");
+                    usernameField.setError("at least 5 characters long");
                 }
                 else if(pass.length()<5){
-                    password.setError("at least 5 characters long");
+                    passwordField.setError("at least 5 characters long");
                 }
                 else {
-                    final ProgressDialog pd = new ProgressDialog(Register.this);
-                    pd.setMessage("Loading...");
-                    pd.show();
-
-                    String url = "https://android-chat-app-e711d.firebaseio.com/users.json";
-
-                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
-                        @Override
-                        public void onResponse(String s) {
-                            Firebase reference = new Firebase("https://android-chat-app-e711d.firebaseio.com/users");
-
-                            if(s.equals("null")) {
-                                reference.child(user).child("password").setValue(pass);
-                                Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
-                            }
-                            else {
-                                try {
-                                    JSONObject obj = new JSONObject(s);
-
-                                    if (!obj.has(user)) {
-                                        reference.child(user).child("password").setValue(pass);
-                                        Toast.makeText(Register.this, "registration successful", Toast.LENGTH_LONG).show();
-                                    } else {
-                                        Toast.makeText(Register.this, "username already exists", Toast.LENGTH_LONG).show();
-                                    }
-
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            pd.dismiss();
-                        }
-
-                    },new Response.ErrorListener(){
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            System.out.println("" + volleyError );
-                            pd.dismiss();
-                        }
-                    });
-
-                    RequestQueue rQueue = Volley.newRequestQueue(Register.this);
-                    rQueue.add(request);
+                    createAccount(user, pass);
                 }
             }
         });
+
+        // the moment the user has been authenticated: change to group view activity
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    // create a new account with the specified email and password, input has to be already sanitized!
+    private void createAccount(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Register.this, "Authentication failed!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            firebaseAuth.removeAuthStateListener(authListener);
+        }
     }
 }
