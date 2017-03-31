ea6a02cb2240839c913c6fc460128e05624a68e7
rafaelk <rafael_kuebler@yahoo.es>
2017-03-31 12:28:54 +0200

    Removed some unused imports

--- app/src/main/java/com/rafael/rpg/rpgmessenger/GroupsActivity.java
+++ app/src/main/java/com/rafael/rpg/rpgmessenger/GroupsActivity.java
@@ -1,7 +1,6 @@
 package com.rafael.rpg.rpgmessenger;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +15,6 @@ import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
